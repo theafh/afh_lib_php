045b153af0bb3c7dc033d04cbb97bb0f0dbfb247
theafh <me@theafh.net>
2015-04-29 21:02:13 +0200

    initial commit of inc.encoding.php with is_utf8() and to_utf8()

---
+++ inc.encoding.php
@@ -0,0 +1,25 @@
+<?
+
+// Testing if a string is UTF8 encoded
+function is_utf8($str){
+	$ret = false;
+	if (mb_detect_encoding($str, 'auto', true) == 'UTF-8'){
+		$ret = true;
+	}
+	return $ret;
+}
+
+// Transform a string to UTF8 encoding
+function to_utf8($str){
+	$ret = $str;
+
+	$enc = mb_detect_encoding($str, 'auto', true);
+	if($enc != 'UTF-8' && $enc != ''){
+		ini_set('mbstring.substitute_character', 'none');
+		$ret = iconv($enc, 'UTF-8//TRANSLIT//IGNORE', $str);
+	}
+	return $ret;
+}
+
+
+?>
